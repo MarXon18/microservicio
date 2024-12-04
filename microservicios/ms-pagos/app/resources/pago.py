e89b2222d6f34c3c758be7778152a2bd0a29b4b3
MarXon18 <164246384+MarXon18@users.noreply.github.com>
2024-12-04 11:41:58 -0300

    Update pago.py

--- microservicios/ms-pagos/app/resources/pago.py
+++ microservicios/ms-pagos/app/resources/pago.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, request
-
+from flask import Blueprint, request, jsonify
+from marshmallow.exceptions import ValidationError
 from app.mapping import PagoSchema
 from app.services import PagoService
 
@@ -7,15 +7,25 @@ pago_bp = Blueprint('pago', __name__)
 pago_schema = PagoSchema()
 pago_service = PagoService()
 
-@pago_bp.route('/pagos/registrar', methods=['POST'])
+@pago_bp.route('/pagos/registrar', methods=["POST", "GET"])
 def registrar_pago():
-    pago = pago_schema.load(request.json)
-    result = pago_service.registrar_pago(pago)
-    if result:
-        status_code = 200 
-    else:
-        status_code = 400
-    return pago_schema.dump(result), status_code
+    data = request.get_json()
+    if not request.is_json:
+        return jsonify({'error': 'La solicitud no contiene datos JSON'}), 400
+
+    try:
+        # Validar y cargar los datos
+        pago = pago_schema.load(data)
+        # Procesar el pago
+        result = pago_service.registrar_pago(pago)
+        if result:
+            return jsonify({'message': 'Pago registrado correctamente'}), 200
+        else:
+            return jsonify({'error': 'Error al registrar el pago'}), 400
+    except ValidationError as ve:
+        return jsonify({'error': 'Error de validación', 'detalles': ve.messages}), 400
+    except Exception as e:
+        return jsonify({'error': 'Error interno del servidor', 'detalles':str(e)}),500
 
 @pago_bp.route('/pagos/cancelar/<int:id>', methods=['PUT'])
 def cancelar_pago(id):
@@ -26,6 +36,10 @@ def cancelar_pago(id):
         result = pago_service.cancelar_pago(pago)
         if result:
             status_code = 200 
+    
+    return pago_schema.dump(result), status_code
+
+
     
     return pago_schema.dump(result), status_code
 
