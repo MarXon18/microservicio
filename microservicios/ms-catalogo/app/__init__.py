29a8601b3eb46a340f7d4c90d93bfc36a9f15d82
MarXon18 <164246384+MarXon18@users.noreply.github.com>
2024-12-04 11:32:36 -0300

    Update __init__.py

--- microservicios/ms-catalogo/app/__init__.py
+++ microservicios/ms-catalogo/app/__init__.py
@@ -1,12 +1,15 @@
 # app/__init__.py
 from flask import Flask, jsonify
 from flask_marshmallow import Marshmallow
+from dotenv import load_dotenv
 import os
 from app.config import config
 from flask_caching import Cache
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from app.config.cache_config import cache_config
+
+load_dotenv()
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -25,11 +28,12 @@ def create_app() -> Flask:
     migrate.init_app(app, db)
     cache.init_app(app, config=cache_config)
     
-    from app.resources.inventarios_bp import inventarios_bp
-    app.register_blueprint(inventarios_bp, url_prefix='/api/v1')
+    from app.resources.catalogo_bp import catalogo_bp
+    app.register_blueprint(catalogo_bp, url_prefix='/api/v1')
     
     @app.shell_context_processor    
     def ctx():
         return {"app": app}
     
     return app
+
